68bad58345e77e48adad3959e80b544b09c7a53d
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-21 18:40:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.name = name;
+		this.age = age;
+	}
 
-class Student extends Person {}
+	get name() {
+		return this.name;
+	}
 
-class Teacher extends Person {}
+	set age(age) {
+		this.age = age;
+	}
+}
+
+class Student extends Person {
+	constructor(name,age) {
+	super(name,age);		
+	}
+	study() {
+		console.log(`${this.name} is studying`);
+	}
+}
+
+class Teacher extends Person {
+	constructor(name,age) {
+	super(name,age);
+	}
+	teach() {
+		console.log(`${this.name} is teaching`);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
